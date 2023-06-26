80fe3aa3074a98c8f1223fab9abbc038eb238282
CuteReimu <415551921@qq.com>
2023-06-26 21:02:25 +0800

    Initial commit

---
+++ translate.py
@@ -0,0 +1,41 @@
+import os
+import re
+from pathlib import Path
+
+from translate_dict import translate_dict
+
+regexp = re.compile(r'>(.+?)<')
+
+
+def translate(s):
+    s0 = s.strip()
+    s1 = translate_dict.get(s0)
+    if s1:
+        return s.replace(s0, s1)
+    matcher = regexp.search(s)
+    while matcher:
+        g = matcher.group(1)
+        s0 = g.strip()
+        s1 = translate_dict.get(s0)
+        if s1:
+            return s[:matcher.start()] + ">" + g.replace(s0, s1) + "<" + s[matcher.end():]
+        matcher = regexp.search(s0, pos=matcher.end())
+    return s
+
+
+for dirpath, _, filenames in os.walk('brendonmay.github.io'):
+    d = Path(dirpath)
+    for filename in filenames:
+        if filename not in ('index.html', 'main.js', 'outputStats.js'):
+            continue
+        filepath = d.joinpath(filename)
+        lines = []
+        with open(filepath, 'r', encoding='utf-8') as f:
+            while True:
+                line = f.readline()
+                if line:
+                    lines.append(translate(line))
+                else:
+                    break
+        with open(filepath, 'w', encoding='utf-8') as f:
+            f.writelines(lines)
---
+++ translate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -o errexit
+python3 translate.py
